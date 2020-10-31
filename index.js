0a65e3824921674318588d6fc14ab63c6d0fa6f7
akash rattan <akashrattan43@Gmail.com>
2020-10-31 16:43:29 -0700

    5

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const person = require('./person.js')
 
-console.log(person.name)
+const person1 = new person('john Doe', 30)
+
+person1.greeting()
--- person.js
+++ person.js
@@ -1,6 +1,13 @@
-const person = {
-    name: 'john Doe',
-    age: 30
+class person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    greeting() {
+        console.log(`MY NAME IS ${this.name} and I am ${this.age}`)
+    }
+    
 }
+
 
 module.exports = person
